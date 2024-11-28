7df10535c67d967ad7c601681f894b1c3f318f28
baobui13 <23520093@gm.uit.edu.vn>
2024-11-28 19:23:07 +0700

    AddPicturePage

--- src/Final_Project/PuzzleGame/MVVM/ViewModels/AddPicturePageViewModel.cs
+++ src/Final_Project/PuzzleGame/MVVM/ViewModels/AddPicturePageViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Enumeration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
 using PuzzleGame.Core;
+using PuzzleGame.Core.Helper;
+using PuzzleGame.MVVM.Models;
 
 namespace PuzzleGame.MVVM.ViewModels
 {
@@ -23,20 +29,66 @@ namespace PuzzleGame.MVVM.ViewModels
             }
         }
 
-        string _name = "HaHaHa";
-        public string Name
+        public string _newPicName { get; set; }
+        public string NewPicName
         {
-            get { return _name; }
+            get { return _newPicName; }
             set
             {
-                _name = value;
-                OnPropertyChanged("Name");
+                _newPicName = value;
+                OnPropertyChanged();
             }
         }
+
+        string _newPicUrl;
+        public string NewPicUrl
+        {
+            get => _newPicUrl;
+            set
+            {
+                _newPicUrl = value;
+                OnPropertyChanged();
+            }
+        }
+
+        BitmapImage _newBitmap;
+        BitmapImage newBitmap
+        {
+            get => _newBitmap;
+            set
+            {
+                _newBitmap = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //Command
+        public RelayCommand<object> ChoosePictureCommand { get; set; }
+        public RelayCommand<object> ChangeNewPictureNameCommand { get; set; }
 
         public AddPicturePageViewModel()
         {
             _wndBgr = defaultColornum1;
+            NewPicUrl = @"/Assets/Imgs/AddPicBackGround.jpg";
+            NewPicName = new string("Hahaha");
+
+            ChoosePictureCommand = new RelayCommand<object>((o) =>
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "All Image Files| *.jpg; *.png; *.jpeg; *.bmp; *.gif;";
+
+                if (dialog.ShowDialog() == true)
+                {
+                    NewPicUrl = dialog.FileName.ToString();
+                }
+            });
+
+            ChangeNewPictureNameCommand = new RelayCommand<object>((o) =>
+            {
+
+
+
+            });
         }
     }
 }
